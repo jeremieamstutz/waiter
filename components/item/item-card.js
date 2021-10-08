e08a46dd108bcd21381e258d9d769d6ae144783e
Jérémie Amstutz <jeremie.amstutz@gmail.com>
2021-10-08 22:32:22 +0200

    increase image size

--- components/item/item-card.js
+++ components/item/item-card.js
@@ -5,24 +5,25 @@ import classes from './item-card.module.css'
 export default function Item({ item }) {
 	return (
 		<div className={classes.container}>
-			<Image
-				className={classes.image}
-				src={item.image}
-				alt={item.name}
-				objectFit="cover"
-				objectPosition="left"
-				width={144}
-				height={192}
-			/>
+			<div className={classes.image}>
+				<Image
+					src={item.image}
+					alt={item.name}
+					objectFit="cover"
+					objectPosition="left"
+					width={176}
+					height={234}
+				/>
+			</div>
 			<h3 className={classes.title}>{item.name}</h3>
 			<p className={classes.description}>{item.description}</p>
 			<p className={classes.details}>
 				<span className={classes.price}>
 					CHF {parseFloat(item.price).toFixed(2)}
 				</span>
-				<span className={classes.grade}>
+				{/* <span className={classes.grade}>
 					{(2 * Math.random() + 3).toFixed(2)}
-				</span>
+				</span> */}
 			</p>
 		</div>
 	)
