37a98605612ec5e5730d0e0c4da6d3725bd32245
Jérémie Amstutz <jeremie.amstutz@gmail.com>
2021-10-10 19:22:43 +0200

    update about page

--- pages/[citySlug]/[restaurantSlug]/about.js
+++ pages/[citySlug]/[restaurantSlug]/about.js
@@ -1,16 +1,53 @@
 import Image from 'next/image'
 
-export default function AboutPage() {
-	const restaurant = { name: 'Holy Cow' }
+import Footer from 'components/layout/footer'
+
+import { getRestaurantsSlugs, getRestaurant } from 'utils/db'
+
+import classes from 'styles/about.module.css'
+
+export default function AboutPage({ restaurant }) {
 	return (
 		<>
-			<h1>About</h1>
-			<Image
-				src={`${process.env.NEXT_PUBLIC_DOMAIN}/api/qrcode?url=${process.env.NEXT_PUBLIC_DOMAIN}/lausanne/holycow`}
-				alt={`${restaurant.name}'s QR code`}
-				width={375}
-				height={375}
-			/>
+			<div className={classes.container}>
+				<h1>About {restaurant.name}</h1>
+				<p>{restaurant.description}</p>
+				<p>{restaurant.address}</p>
+				<Image
+					src={`${process.env.NEXT_PUBLIC_DOMAIN}/api/qrcode?url=${process.env.NEXT_PUBLIC_DOMAIN}/lausanne/holycow`}
+					alt={`${restaurant.name}'s QR code`}
+					width={375}
+					height={375}
+				/>
+			</div>
+			<Footer />
 		</>
 	)
+}
+
+export async function getStaticPaths() {
+	const restaurantsSlug = await getRestaurantsSlugs()
+	return {
+		paths: restaurantsSlug.map((restaurant) => ({ params: restaurant })),
+		fallback: 'blocking',
+	}
+}
+
+export async function getStaticProps({ params }) {
+	const { restaurantSlug, citySlug } = params
+	const restaurant = await getRestaurant({ restaurantSlug, citySlug })
+
+	console.log(restaurant)
+	if (!restaurant) {
+		return {
+			notFound: true,
+		}
+	}
+
+	return {
+		props: {
+			restaurant: restaurant,
+		},
+		revalidate: 60
+	}
 }
