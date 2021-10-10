9209e5c92aee724e9b4fd596fc21e0c4be0cb414
Jérémie Amstutz <jeremie.amstutz@gmail.com>
2021-10-10 19:57:36 +0200

    add terms page

--- components/layout/footer.js
+++ components/layout/footer.js
@@ -3,8 +3,24 @@ import Link from 'next/link'
 import classes from './footer.module.css'
 
 export default function Footer() {
-    return <footer className={classes.footer}>
-        <h3 className={classes.title}>Powered by <Link href="/"><a className={classes.link}>waiter.so</a></Link></h3>
-        {/* <p className={classes.slogan}>Commandez en ligne. Dégustez sur place.</p> */}
-    </footer>
+	return (
+		<footer className={classes.footer}>
+			<h3 className={classes.title}>
+				Powered by{' '}
+				<Link href="/">
+					<a className={classes.brand}>waiter.so</a>
+				</Link>
+			</h3>
+            <div className={classes.links}>
+			<Link href="/terms">
+				<a className={classes.link}>Terms</a>
+			</Link>
+            <span className={classes.separator}>·</span>
+            <Link href="/terms">
+				<a className={classes.link}>Privacy</a>
+			</Link>
+            </div>
+			{/* <p className={classes.slogan}>Commandez en ligne. Dégustez sur place.</p> */}
+		</footer>
+	)
 }
