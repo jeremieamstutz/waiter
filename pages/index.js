6601fb23bad0e00fddd55df11c1dc0c8f73543f4
Jérémie Amstutz <jeremie.amstutz@gmail.com>
2021-10-21 01:34:54 +0200

    misc

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Link from 'next/link'
 import { getAllRestaurants } from 'utils/db'
-import Image from 'next/image'
 
 import RestaurantCard from 'components/restaurant/restaurant-card'
 import Header from 'components/layout/header'
@@ -14,48 +13,46 @@ export default function Home({ restaurants }) {
 		<>
 			<div className={classes.container}>
 				<h1>Waiter</h1>
+				<p>Commandez en ligne. Régalez vous sur place.</p>
 				{/* <div>Mon restaurant</div> */}
-				<h2 style={{margin: 0}}>Recommandations</h2>
-				<div style={{ display: 'flex', gap: '1rem', margin: '0 -1rem', padding: '1rem', overflow: 'auto', scrollSnapType: 'x mandatory', scrollPadding: '1rem'}}>
+				<h2 style={{ margin: 0 }}>Recommandations</h2>
+				<div
+					style={{
+						display: 'flex',
+						gap: '1rem',
+						margin: '0 -1rem',
+						padding: '1rem',
+						overflow: 'auto',
+						scrollSnapType: 'x mandatory',
+						scrollPadding: '1rem',
+					}}
+				>
 					{restaurants.map((restaurant, index) => (
-						<Link
-							href={{
-								pathname: '/[citySlug]/[restaurantSlug]',
-								query: {
-									citySlug: restaurant.citySlug,
-									restaurantSlug: restaurant.restaurantSlug,
-								},
-							}}
-							key={index}
-						>
-							<a>
-								<RestaurantCard restaurant={restaurant}/>
-							</a>
-						</Link>
+						<RestaurantCard restaurant={restaurant} key={index} />
 					))}
 				</div>
-				<h2 style={{margin: 0}}>Burgers</h2>
-				<div style={{ display: 'flex', gap: '1rem', margin: '0 -1rem', padding: '1rem', overflow: 'auto', scrollSnapType: 'x mandatory', scrollPadding: '1rem'}}>
+				<h2 style={{ margin: 0 }}>Burgers</h2>
+				<div
+					style={{
+						display: 'flex',
+						gap: '1rem',
+						margin: '0 -1rem',
+						padding: '1rem',
+						overflow: 'auto',
+						scrollSnapType: 'x mandatory',
+						scrollPadding: '1rem',
+					}}
+				>
 					{restaurants.map((restaurant, index) => (
-						<Link
-							href={{
-								pathname: '/[citySlug]/[restaurantSlug]',
-								query: {
-									citySlug: restaurant.citySlug,
-									restaurantSlug: restaurant.restaurantSlug,
-								},
-							}}
-							key={index}
-						>
-							<a>
-								<RestaurantCard restaurant={restaurant}/>
-							</a>
-						</Link>
+						<RestaurantCard restaurant={restaurant} key={index} />
 					))}
 				</div>
 				<div className={classes.cta}>
 					<h3>Et votre restaurant ?</h3>
-					<p>Découvrez les nombreux avantages que Waiter peut vous offrir</p>
+					<p>
+						Découvrez les nombreux avantages que Waiter peut vous
+						offrir
+					</p>
 					<Link
 						href={{
 							pathname: '/advantages',
