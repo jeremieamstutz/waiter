b8923c2a0ffbb5bc111abb32b3d476b2a9d827b7
Jérémie Amstutz <jeremie.amstutz@gmail.com>
2021-11-06 23:43:41 +0100

    add possibility to create restaurant

---
+++ components/restaurant/restaurant-form.js
@@ -0,0 +1,187 @@
+import { useRouter } from 'next/router'
+import { Formik, Form, ErrorMessage } from 'formik'
+import * as Yup from 'yup'
+import axios from 'axios'
+
+import { Input, Textarea, Select } from 'components/ui/form-items'
+import ImagePicker from 'components/ui/image-picker'
+
+import classes from './restaurant-form.module.css'
+
+export default function RestaurantForm({ restaurant }) {
+	const router = useRouter()
+
+	return (
+		<Formik
+			initialValues={{
+				image: restaurant?.image || '',
+				name: restaurant?.name || '',
+				description: restaurant?.description || '',
+				cuisine: restaurant?.cuisine || '',
+				phone: restaurant?.phone || '',
+				street: restaurant?.street || '',
+				streetNumber: restaurant?.streetNumber || '',
+				postalCode: restaurant?.postalCode || '',
+				city: restaurant?.city || '',
+				region: restaurant?.region || '',
+				country: restaurant?.country || '',
+			}}
+			validationSchema={Yup.object({
+				image: Yup.string().required('You must add an image'),
+				name: Yup.string()
+					.min(3, 'Must be 3 characters or more')
+					.max(30, 'Must be 30 characters or less')
+					.required('Name is required'),
+				description: Yup.string().max(
+					500,
+					'Must be 500 characters or less',
+				),
+				cuisine: Yup.string()
+					.min(3, 'Must be 3 characters or more')
+					.max(30, 'Must be 30 characters or less')
+					.required('Cuisine is required'),
+				phone: Yup.string().min(8, 'Must be 8 characters or more'),
+				street: Yup.string()
+					.min(3, 'Must be 3 characters or more')
+					.max(30, 'Must be 30 characters or less')
+					.required('Street is required'),
+				streetNumber: Yup.number()
+					.positive('Must be positive')
+					.integer('Must be an integer'),
+				postalCode: Yup.number()
+					.positive('Must be positive')
+					.integer('Must be an integer')
+					.required('Postal code is required'),
+				city: Yup.string()
+					.min(3, 'Must be 3 characters or more')
+					.max(30, 'Must be 30 characters or less')
+					.required('City is required'),
+				region: Yup.string()
+					.min(3, 'Must be 3 characters or more')
+					.max(30, 'Must be 30 characters or less'),
+				country: Yup.string()
+					.min(3, 'Must be 3 characters or more')
+					.max(30, 'Must be 30 characters or less')
+					.required('Country is required'),
+			})}
+			onSubmit={async (values) => {
+				let response
+				if (!restaurant) {
+					response = await axios.post('/api/restaurants', {
+						restaurant: {
+							...values,
+						},
+					})
+				} else {
+					response = await axios.put(
+						`/api/restaurants/${restaurant.id}`,
+						{
+							restaurant: {
+								...values,
+							},
+						},
+					)
+				}
+				// await sleep(2000)
+				const data = response.data
+				router.push({
+					pathname: '/[restaurantSlug]',
+					query: {
+						restaurantSlug: data.restaurant.slug,
+					},
+				})
+			}}
+		>
+			{({ values, setFieldValue, isSubmitting }) => (
+				<Form className={classes.form}>
+					<ImagePicker
+						url={values.image}
+						setUrl={(url) => setFieldValue('image', url)}
+					/>
+					<ErrorMessage name="image" />
+					<Input
+						name="name"
+						type="text"
+						placeholder="Name"
+						autoComplete="off"
+					/>
+					<Textarea
+						name="description"
+						placeholder="Description"
+						rows={3}
+					/>
+					<Select name="cuisine">
+						<option value="" disabled>
+							Cuisine
+						</option>
+						<option value="Américaine">Américaine</option>
+						<option value="Japonaise">Japonaise</option>
+						<option value="Suisse">Suisse</option>
+						<option value="Française">Française</option>
+						<option value="Italienne">Italienne</option>
+						<option value="Espagnole">Espagnole</option>
+						<option value="Chinoise">Chinoise</option>
+						<option value="Thaïlandaise">Thaïlandaise</option>
+						<option value="Thaïlandaise">Vietnamienne</option>
+						<option value="Mexicaine">Mexicaine</option>
+						<option value="Indienne">Indienne</option>
+						<option value="Portugaise">Portugaise</option>
+						<option value="Grecque">Grecque</option>
+						<option value="Asiatique">Asiatique</option>
+						<option value="Africaine">Africaine</option>
+						<option value="Méditerranéenne">Méditerranéenne</option>
+					</Select>
+					<Input
+						name="phone"
+						type="tel"
+						placeholder="Phone"
+						autoComplete="off"
+					/>
+					<Input
+						name="street"
+						type="text"
+						placeholder="Street"
+						autoComplete="off"
+					/>
+					<Input
+						name="streetNumber"
+						type="number"
+						placeholder="Number"
+						autoComplete="off"
+					/>
+					<Input
+						name="postalCode"
+						type="number"
+						placeholder="Postal Code"
+						autoComplete="off"
+					/>
+					<Input
+						name="city"
+						type="text"
+						placeholder="City"
+						autoComplete="off"
+					/>
+					<Input
+						name="region"
+						type="text"
+						placeholder="Region"
+						autoComplete="off"
+					/>
+					<Select name="country">
+						<option value="" disabled>
+							Country
+						</option>
+						<option value="Switzerland">Suisse</option>
+					</Select>
+					<button type="submit" className="secondary">
+						{isSubmitting
+							? 'Loading....'
+							: restaurant
+							? 'Update'
+							: 'Save'}
+					</button>
+				</Form>
+			)}
+		</Formik>
+	)
+}
--- pages/api/restaurants/[restaurantId]/index.js
+++ pages/api/restaurants/[restaurantId]/index.js
@@ -1,23 +1,128 @@
-import { getSession } from 'next-auth/client'
+import axios from 'axios'
+import { getSession } from 'next-auth/react'
+import slugify from 'slugify'
+
+import { query } from 'utils/db'
 import statusCodes from 'utils/statusCodes'
 
 export default async function handler(req, res) {
 	const {
 		query: { restaurantId },
 		method,
-		body
+		body,
 	} = req
 
 	const session = await getSession({ req })
 
-	console.log(session, body)
-
 	switch (method) {
-		case 'GET':
-			res.status(statusCodes.ok)
+		case 'GET': {
+			const restaurant = await getRestaurant({ restaurantId })
+			res.status(statusCodes.ok).json({ restaurant })
+			break
+		}
+		case 'PUT':
+			const { restaurant } = req.body
+
+			const session = await getSession({ req })
+
+			restaurant.id = restaurantId
+			restaurant.slug = slugify(restaurant.name, { lower: true })
+			restaurant.ownerId = session.user.id
+			restaurant.address = `${restaurant.street} ${restaurant.streetNumber}, ${restaurant.postalCode} ${restaurant.city}, ${restaurant.country}`
+
+			const response = await axios.get(
+				`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
+					restaurant.address,
+				)}&key=${process.env.GOOGLE_API_KEY}`,
+			)
+			const data = response.data
+			if (!data || data.status === 'ZERO_RESULTS') {
+				res.status(statusCodes.badRequest).json({
+					error: 'Address not found',
+				})
+			}
+			const coordinates = data.results[0].geometry.location
+			restaurant.latitude = coordinates.lat
+			restaurant.longitude = coordinates.lng
+
+			const newRestaurant = await updateRestaurant({ restaurant })
+
+			res.status(statusCodes.ok).json({ restaurant: newRestaurant })
 			break
 		default:
 			res.status(statusCodes.methodNotAllowed).end()
 			break
 	}
+}
+
+export async function updateRestaurant({ restaurant }) {
+	const result = await query(
+		`
+        UPDATE restaurants
+		SET slug = $2, owner_id = $3, image = $4, name = $5, description = $6, cuisine = $7, phone = $8, address = $9, street = $10, street_number = $11, postal_code = $12, city = $13, region = $14, country = $15, latitude = $16, longitude = $17
+		WHERE restaurants.id = $1
+        RETURNING *
+    `,
+		[
+			restaurant.id,
+			restaurant.slug,
+			restaurant.ownerId,
+			restaurant.image,
+			restaurant.name,
+			restaurant.description,
+			restaurant.cuisine,
+			restaurant.phone,
+			restaurant.address,
+			restaurant.street,
+			restaurant.streetNumber,
+			restaurant.postalCode,
+			restaurant.city,
+			restaurant.region,
+			restaurant.country,
+			restaurant.latitude,
+			restaurant.longitude,
+		],
+	)
+	return result.rows[0]
+}
+
+export async function getRestaurant({ restaurantSlug, restaurantId }) {
+	if (restaurantId) {
+		const result = await query(
+			`SELECT *, restaurants.slug AS "restaurantSlug", street_number AS "streetNumber", postal_code AS "postalCode" FROM restaurants 
+			WHERE restaurants.id = $1`,
+			[restaurantId],
+		)
+		return result.rows[0]
+	}
+
+	if (restaurantSlug) {
+		const result = await query(
+			`SELECT *, restaurants.slug AS "restaurantSlug", street_number AS "streetNumber", postal_code AS "postalCode" FROM restaurants 
+			WHERE restaurants.slug = $1`,
+			[restaurantSlug],
+		)
+		return result.rows[0]
+	}
+	return
+}
+
+export async function getRestaurantItems({ restaurantSlug }) {
+	const result = await query(
+		`SELECT items.* FROM items 
+		JOIN restaurants ON restaurants.id = items.restaurant_id  
+		WHERE restaurants.slug = $1`,
+		[restaurantSlug],
+	)
+	return result.rows
+}
+
+export async function getRestaurantCategories({ restaurantSlug }) {
+	const result = await query(
+		`SELECT categories.* FROM categories 
+		JOIN restaurants ON restaurants.id = categories.restaurant 
+		WHERE restaurants.slug = $1`,
+		[restaurantSlug],
+	)
+	return result.rows
 }
