d18930e5f5c678ba6a62eb88863d7081144e881f
Jérémie Amstutz <jeremie.amstutz@gmail.com>
2021-11-09 13:23:50 +0100

    add scroll restauration to restaurants

--- components/restaurant/restaurant-list.js
+++ components/restaurant/restaurant-list.js
@@ -1,8 +1,22 @@
 import RestaurantCard from 'components/restaurant/restaurant-card'
+import useScrollRestoration from 'hooks/useScrollRestauration'
+import { useRouter } from 'next/router'
+import { useRef } from 'react'
+import slugify from 'slugify'
 
 import classes from './restaurant-list.module.css'
 
 export default function RestaurantList({ list, restaurants }) {
+	const router = useRouter()
+	const listRef = useRef()
+
+	useScrollRestoration(
+		listRef,
+		`${router.asPath === '/' ? '/home' : router.asPath}/${slugify(
+			list.name,
+			{ lower: true },
+		)}`,
+	)
 
 	return (
 		<section className={classes.container}>
@@ -12,7 +26,7 @@ export default function RestaurantList({ list, restaurants }) {
 					<p className={classes.description}>{list.description}</p>
 				)}
 			</div>
-			<div className={classes.list}>
+			<div className={classes.list} ref={listRef}>
 				{restaurants.map((restaurant, index) => (
 					<RestaurantCard restaurant={restaurant} key={index} />
 				))}
