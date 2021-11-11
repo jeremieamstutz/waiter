cd135843f1ee217b04d0137db796ca9acef23099
Jérémie Amstutz <jeremie.amstutz@gmail.com>
2021-11-11 21:04:59 +0100

    fix typo

--- components/misc/desktop-unavailable.js
+++ components/misc/desktop-unavailable.js
@@ -6,7 +6,9 @@ export default function DesktopUnavailable({ url }) {
 	return (
 		<div className={classes.container}>
 			<Image
-				src={`${process.env.NEXT_PUBLIC_DOMAIN}/api/qrcode?url=${process.env.NEXT_PUBLIC_DOMAIN + url}`}
+				src={`${process.env.NEXT_PUBLIC_DOMAIN}/api/qrcode?url=${
+					process.env.NEXT_PUBLIC_DOMAIN + url
+				}`}
 				alt="Page's QR code"
 				width={280}
 				height={280}
@@ -14,12 +16,13 @@ export default function DesktopUnavailable({ url }) {
 			/>
 			<h1 className={classes.title}>Indisponible</h1>
 			<p className={classes.text}>
-				La version ordinateur/tablette de cette page n`&apos;est{' '}
+				La version ordinateur/tablette de cette page n&apos;est{' '}
 				<span style={{ color: '#000' }}>pas encore</span> disponible.
 			</p>
 			<p className={classes.text}>
-				Veuillez la consulter depuis un <span style={{ color: '#000' }}>smartphone</span> en scannant le <span style={{ color: '#000' }}>code
-				QR</span> ci-dessus.
+				Veuillez la consulter depuis un{' '}
+				<span style={{ color: '#000' }}>smartphone</span> en scannant le{' '}
+				<span style={{ color: '#000' }}>code QR</span> ci-dessus.
 			</p>
 		</div>
 	)
