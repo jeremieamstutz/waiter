7533eee7132a872cbf0029523a595163592890c6
Jérémie Amstutz <jeremie.amstutz@gmail.com>
2021-11-11 19:11:08 +0100

    add meta description to the home page

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,32 @@
 import Link from 'next/link'
+import Head from 'next/head'
+
 import { getAllRestaurants } from 'pages/api/restaurants'
+import { groupBy } from 'utils/processing'
 
 import Container from 'components/layout/container'
 import RestaurantList from 'components/restaurant/restaurant-list'
 import Header from 'components/layout/header'
 
 import classes from 'styles/home.module.css'
-import { groupBy } from 'utils/processing'
 
 export default function HomePage({ restaurants }) {
 	return (
 		<>
+			<Head>
+				<title>Waiter</title>
+				<meta
+					name="description"
+					content="De choix du restaurant, jusqu’au règlement de l’addition, en passant par la commande, Waiter est là pour vous."
+				/>
+				<meta property="og:title" content="Waiter" />
+				<meta
+					property="og:description"
+					content="De choix du restaurant, jusqu’au règlement de l’addition, en passant par la commande, Waiter est là pour vous."
+				/>
+				<meta property="og:url" content="https://www.waiter.so" />
+				<meta property="og:type" content="restaurants" />
+			</Head>
 			<Container>
 				<h1>Accueil</h1>
 				{/* <p>Commandez en ligne. Régalez vous sur place.</p> */}
