abfe979d9fc41d7e3bf9c7bfcbf0f4228002fbb5
Jérémie Amstutz <jeremie.amstutz@gmail.com>
2021-12-18 01:11:18 +0100

    change header from sticky to fixed

--- components/layout/header.js
+++ components/layout/header.js
@@ -1,14 +1,30 @@
+import { useEffect, useRef } from 'react'
+
 import Menu from './menu'
 import AddToHomeScreen from 'components/cta/add-to-home-screen'
 
 import classes from './header.module.css'
 
 export default function Header({ children }) {
+	const spacerRef = useRef()
+	const headerRef = useRef()
+
+	useEffect(() => {
+		if (headerRef.current) {
+			const rect = headerRef.current.getBoundingClientRect()
+			spacerRef.current.style.height = rect.height + 'px'
+		}
+	})
+
 	return (
-		<div className={classes.header}>
-			{children}
-			{/* <AddToHomeScreen /> */}
-			<Menu />
-		</div>
+		<>
+			<div className={classes.header} ref={headerRef}>
+				{children}
+				{/* <AddToHomeScreen /> */}
+				<Menu />
+			</div>
+
+			<div ref={spacerRef} className={classes.spacer} />
+		</>
 	)
 }
