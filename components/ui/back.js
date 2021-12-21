d731cd74ca3061678c221b045c66c412e7c5e25e
Jérémie Amstutz <jeremie.amstutz@gmail.com>
2021-12-21 01:46:24 +0100

    misc

--- components/ui/back.js
+++ components/ui/back.js
@@ -5,25 +5,17 @@ import CloseButton from 'components/buttons/close'
 
 import classes from './back.module.css'
 import { useHistory } from 'contexts/history'
+import { useRouter } from 'next/router'
 
 export default function BackButton() {
-	const [showBackButton, setShowBackButton] = useState(false)
-    const { history, back } = useHistory()
-
-	useEffect(() => {
-		if (history.length > 1) {
-			setShowBackButton(true)
-		}
-	}, [history])
+	const router = useRouter()
 
 	return (
-		showBackButton && (
-			<Portal selector="#back">
-				<CloseButton
-					className={`${classes.button} secondary`}
-					onClick={() => back()}
-				/>
-			</Portal>
-		)
+		<Portal selector="#back">
+			<CloseButton
+				className={`${classes.button} secondary`}
+				onClick={() => router.back()}
+			/>
+		</Portal>
 	)
 }
