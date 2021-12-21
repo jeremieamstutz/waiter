505fb8b2b7d5c188d6dac066b9db70cdffe0e850
Jérémie Amstutz <jeremie.amstutz@gmail.com>
2021-12-21 14:25:50 +0100

    add bigger touch area

--- components/ui/back.js
+++ components/ui/back.js
@@ -1,21 +1,14 @@
-import { useState, useEffect } from 'react'
-
-import Portal from 'components/misc/portal'
 import CloseButton from 'components/buttons/close'
 
 import classes from './back.module.css'
-import { useHistory } from 'contexts/history'
 import { useRouter } from 'next/router'
 
 export default function BackButton() {
 	const router = useRouter()
 
 	return (
-		<Portal selector="#back">
-			<CloseButton
-				className={`${classes.button} secondary`}
-				onClick={() => router.back()}
-			/>
-		</Portal>
+		<div className={classes.container} onClick={() => router.back()}>
+			<CloseButton className={`${classes.button} secondary`} />
+		</div>
 	)
 }
