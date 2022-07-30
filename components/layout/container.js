a665bae3949b66a4adef12b031216f035d54156a
Jérémie Amstutz <jeremie.amstutz@gmail.com>
2022-07-30 14:01:08 +0200

    cleaning

--- components/layout/container.js
+++ components/layout/container.js
@@ -1,52 +1,17 @@
 import { motion } from 'framer-motion'
 import { fadeIn } from 'animations'
-import { useRouter } from 'next/router'
-import { useEffect, useRef } from 'react'
-import { useAnimation } from 'contexts/animate'
-import useScrollRestoration from 'hooks/useScrollRestauration'
 
 import classes from './container.module.css'
 
 export default function Container({ children, ...props }) {
-	const router = useRouter()
-
-	// const [shouldAnimate, setShouldAnimate] = useAnimation()
-
-	const shouldAnimate = false
-
-	// useEffect(() => {
-	// 	let popState = false
-	// 	router.beforePopState(() => {
-	// 		console.log('BEFORE POP')
-	// 		popState = true
-	// 		setShouldAnimate(false)
-	// 		return true
-	// 	})
-
-	// 	router.events.on('routeChangeStart', () => {
-	// 		console.log(popState)
-	// 		if (!popState) {
-	// 			// setShouldAnimate(true)
-	// 		}
-	// 	})
-	// }, [])
-
-	// const containerRef = useRef()
-
-	// useScrollRestoration(
-	// 	containerRef,
-	// 	router.asPath,
-	// )
-
 	return (
 		<motion.div
-			className={`${classes.container} ${props.className}`}
-			initial={shouldAnimate ? 'initial' : false}
+			className={classes.container}
+			initial="initial"
 			animate="animate"
 			exit="initial"
 			variants={fadeIn}
 			{...props}
-			// ref={containerRef}
 		>
 			{children}
 		</motion.div>
