b8debb533bbb31a6b33ab039723cc7b0a6be5cab
Jérémie Amstutz <jeremie.amstutz@gmail.com>
2022-08-13 14:11:53 +0200

    create orders page

---
+++ pages/orders.js
@@ -0,0 +1,27 @@
+import Container from 'components/layout/container'
+import Footer from 'components/layout/footer'
+import Header from 'components/layout/header'
+import Main from 'components/layout/main'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+
+export default function OrdersPage() {
+	return (
+		<>
+			<Container>
+				<Header />
+				<Main>
+					<h1>Orders</h1>
+				</Main>
+				<Footer />
+			</Container>
+		</>
+	)
+}
+
+export async function getServerSideProps({ locale }) {
+	return {
+		props: {
+			...(await serverSideTranslations(locale, ['common'])),
+		},
+	}
+}
