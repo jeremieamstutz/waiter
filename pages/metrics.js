9fa09f659c7213ae3a173bda617406583caf0194
Jérémie Amstutz <jeremie.amstutz@gmail.com>
2022-08-13 14:05:24 +0200

    create metrics page

--- pages/metrics.js
+++ pages/metrics.js
@@ -1,11 +1,170 @@
+// import {
+// 	LineChart,
+// 	Line,
+// 	YAxis,
+// 	XAxis,
+// 	Tooltip,
+// 	ReferenceLine,
+// 	ResponsiveContainer,
+// } from 'recharts'
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+
 import Container from 'components/layout/container'
+import Footer from 'components/layout/footer'
 import Header from 'components/layout/header'
-import HorizontalRule from 'components/ui/horizontal-rule'
+import Main from 'components/layout/main'
 
 import classes from 'styles/metrics.module.css'
 import { query } from 'utils/db'
+import { useState } from 'react'
 
-export default function MetricsPage({
+const data = [
+	{
+		date: new Date(2022, 0, 1),
+		users: 0,
+	},
+	{
+		date: new Date(2022, 0, 2),
+		users: 1,
+	},
+	{
+		date: new Date(2022, 0, 3),
+		users: 4,
+	},
+	{
+		date: new Date(2022, 0, 4),
+		users: 8,
+	},
+	{
+		date: new Date(2022, 0, 5),
+		users: 19,
+	},
+	{
+		date: new Date(2022, 0, 6),
+		users: 29,
+	},
+	{
+		date: new Date(2022, 0, 7),
+		users: 88,
+	},
+	{
+		date: new Date(2022, 0, 8),
+		users: 19,
+	},
+	{
+		date: new Date(2022, 0, 10),
+		users: 29,
+	},
+	{
+		date: new Date(2022, 0, 11),
+		users: 88,
+	},
+]
+
+const CustomTooltip = ({ active, payload, label }) => {
+	if (active && payload && payload.length) {
+		return (
+			<div style={{ color: '#666' }}>
+				{label.toLocaleDateString('en-US', {
+					weekday: 'long',
+					year: 'numeric',
+					month: 'long',
+					day: 'numeric',
+				})}
+			</div>
+		)
+	}
+
+	return null
+}
+
+// function Chart({ name, key, value, data }) {
+// 	const initialValue = data[data.length - 1].users
+// 	const [selectedRange, setSelectedRange] = useState('1W')
+// 	const [currentValue, setCurrentValue] = useState(initialValue)
+// 	// const [position, setPosition] = useState()
+// 	return (
+// 		<div
+// 			style={{
+// 				display: 'flex',
+// 				flexDirection: 'column',
+// 				gap: '1rem',
+// 				background: '#eee',
+// 				borderRadius: '1rem',
+// 				padding: '1.5rem',
+// 			}}
+// 		>
+// 			<h2 style={{ margin: 0, fontSize: '1.5rem' }}>{name}</h2>
+// 			<div style={{ fontSize: '1.5rem' }}>
+// 				{currentValue === undefined ? initialValue : currentValue}
+// 			</div>
+// 			<ResponsiveContainer width="100%" height={200}>
+// 				<LineChart
+// 					data={data}
+// 					onMouseMove={(event) => {
+// 						setCurrentValue(event?.activePayload?.[0]?.value)
+// 					}}
+// 					onMouseLeave={() => setCurrentValue(initialValue)}
+// 					margin={{ top: 32 }}
+// 				>
+// 					<Line
+// 						dataKey="users"
+// 						type="linear"
+// 						strokeWidth={3}
+// 						stroke="var(--color-primary)"
+// 						dot={false}
+// 					/>
+// 					{/* <Tooltip
+// 						wrapperStyle={{ visibility: 'visible' }}
+// 						position={{ x: 0, y: -84 }}
+// 						content={
+// 						}
+// 					/> */}
+// 					<Tooltip
+// 						cursor={{ stroke: '#ccc', strokeWidth: 2 }}
+// 						content={CustomTooltip}
+// 						// coordinate={{ x: 0, y: 0 }}
+// 						position={{ y: 0 }}
+// 					/>
+// 					<ReferenceLine
+// 					// x={position}
+// 					// label="SALUT"
+// 					// strokeWidth={3}
+// 					// stroke="var(--color-primary)"
+// 					/>
+// 					<XAxis
+// 						hide
+// 						dataKey="date"
+// 						tickFormatter={(tickItem) => tickItem.getDate()}
+// 					/>
+// 					<YAxis hide domain={['dataMin', 'dataMax']} width={20} />
+// 				</LineChart>
+// 			</ResponsiveContainer>
+// 			<nav style={{ display: 'flex', gap: '0.5rem' }}>
+// 				{['1W', '1M', '3M', '1Y', 'ALL'].map((range) => (
+// 					<button
+// 						key={range}
+// 						onClick={() => setSelectedRange(range)}
+// 						style={{
+// 							flex: 1,
+// 							color: selectedRange === range ? 'white' : '#222',
+// 							background: 'none',
+// 							padding: 0,
+// 							background:
+// 								selectedRange === range
+// 									? 'var(--color-primary)'
+// 									: 'transparent',
+// 						}}
+// 					>
+// 						{range}
+// 					</button>
+// 				))}
+// 			</nav>
+// 		</div>
+// 	)
+// }
+
+export default function OpenPage({
 	newSince,
 	totalUsers,
 	newUsers,
@@ -17,104 +176,112 @@ export default function MetricsPage({
 	return (
 		<>
 			<Container>
-				<h1>Metrics</h1>
-				<div className={classes.metric}>
-					<h2 className={classes.title}>Total users</h2>
-					<div className={classes.body}>
-						<div>
-							<span className={classes.value}>{totalUsers}</span>
-							{/* <small className={classes.previous}>
-								from 504 last month
-							</small> */}
-						</div>
-						{/* <span className={classes.trend}>+15%</span> */}
+				<Header />
+				<Main>
+					<h1>Metrics</h1>
+					<div
+						style={{
+							display: 'grid',
+							gap: '1.5rem',
+							gridTemplateColumns:
+								'repeat(auto-fill, minmax(25rem, 1fr))',
+						}}
+					>
+						{/* <Chart name="Users" data={data} />
+						<Chart name="Restaurants" data={data} />
+						<Chart name="Bookings" data={data} />
+						<Chart name="Transactions" data={data} />
+						<Chart name="Revenue" data={data} /> */}
 					</div>
-				</div>
-				<div className={classes.metric}>
-					<h2 className={classes.title}>New users</h2>
-					<p className={classes.comment}>
-						Since last {newSince} days
-					</p>
-					<div className={classes.body}>
-						<div>
-							<span className={classes.value}>+{newUsers}</span>
 
-							{/* <small className={classes.previous}>from 30</small> */}
+					{/* <div className={classes.metric}>
+						<h2 className={classes.title}>Total users</h2>
+						<div className={classes.body}>
+							<div>
+								<span className={classes.value}>
+									{totalUsers}
+								</span>
+							</div>
 						</div>
-						{/* <span className={classes.trend}>+100%</span> */}
 					</div>
-				</div>
-				<HorizontalRule />
-				<div className={classes.metric}>
-					<h2 className={classes.title}>Total restaurants</h2>
-					<div className={classes.body}>
-						<div>
-							<span className={classes.value}>
-								{totalRestaurants}
-							</span>
-							{/* <small className={classes.previous}>from 504</small> */}
+					<div className={classes.metric}>
+						<h2 className={classes.title}>New users</h2>
+						<p className={classes.comment}>
+							Since last {newSince} days
+						</p>
+						<div className={classes.body}>
+							<div>
+								<span className={classes.value}>
+									+{newUsers}
+								</span>
+							</div>
 						</div>
-						{/* <span className={classes.trend}>+15%</span> */}
 					</div>
-				</div>
-				<div className={classes.metric}>
-					<h2 className={classes.title}>New restaurants</h2>
-					<p className={classes.comment}>
-						Since last {newSince} days
-					</p>
-					<div className={classes.body}>
-						<div>
-							<span className={classes.value}>
-								+{newRestaurants}
-							</span>
-							{/* <small className={classes.previous}>from 30</small> */}
+					<HorizontalRule />
+					<div className={classes.metric}>
+						<h2 className={classes.title}>Total restaurants</h2>
+						<div className={classes.body}>
+							<div>
+								<span className={classes.value}>
+									{totalRestaurants}
+								</span>
+							</div>
 						</div>
-						{/* <span className={classes.trend}>+100%</span> */}
 					</div>
-				</div>
-				<HorizontalRule />
-				<div className={classes.metric}>
-					<h2 className={classes.title}>Total items</h2>
-					<div className={classes.body}>
-						<div>
-							<span className={classes.value}>{totalItems}</span>
-							{/* <small className={classes.previous}>from 504</small> */}
+					<div className={classes.metric}>
+						<h2 className={classes.title}>New restaurants</h2>
+						<p className={classes.comment}>
+							Since last {newSince} days
+						</p>
+						<div className={classes.body}>
+							<div>
+								<span className={classes.value}>
+									+{newRestaurants}
+								</span>
+							</div>
 						</div>
-						{/* <span className={classes.trend}>+15%</span> */}
 					</div>
-				</div>
-				<div className={classes.metric}>
-					<h2 className={classes.title}>New items</h2>
-					<p className={classes.comment}>
-						Since last {newSince} days
-					</p>
-					<div className={classes.body}>
-						<div>
-							<span className={classes.value}>+{newItems}</span>
-
-							{/* <small className={classes.previous}>from 30</small> */}
+					<HorizontalRule />
+					<div className={classes.metric}>
+						<h2 className={classes.title}>Total items</h2>
+						<div className={classes.body}>
+							<div>
+								<span className={classes.value}>
+									{totalItems}
+								</span>
+							</div>
 						</div>
-						{/* <span className={classes.trend}>+100%</span> */}
 					</div>
-				</div>
-				<HorizontalRule />
-				<div className={classes.metric}>
-					<h2 className={classes.title}>Recurring revenue</h2>
-					<div className={classes.body}>
-						<div>
-							<span className={classes.value}>0 CHF</span>
-							{/* <small className={classes.previous}>from 30</small> */}
+					<div className={classes.metric}>
+						<h2 className={classes.title}>New items</h2>
+						<p className={classes.comment}>
+							Since last {newSince} days
+						</p>
+						<div className={classes.body}>
+							<div>
+								<span className={classes.value}>
+									+{newItems}
+								</span>
+							</div>
 						</div>
-						{/* <span className={classes.trend}>+100%</span> */}
 					</div>
-				</div>
+					<HorizontalRule />
+					<div className={classes.metric}>
+						<h2 className={classes.title}>Recurring revenue</h2>
+						<div className={classes.body}>
+							<div>
+								<span className={classes.value}>0 CHF</span>
+							</div>
+						</div>
+					</div> */}
+				</Main>
+				<Footer />
 			</Container>
-			<Header />
 		</>
 	)
 }
 
-export async function getStaticProps() {
+export async function getStaticProps({ locale }) {
 	const newSince = 7 // days
 	const totalUsers = +(await query(`SELECT COUNT(*) FROM users`)).rows[0]
 		.count
@@ -152,6 +319,7 @@ export async function getStaticProps() {
 			newRestaurants,
 			totalItems,
 			newItems,
+			...(await serverSideTranslations(locale, ['common'])),
 		},
 		revalidate: 5,
 	}
