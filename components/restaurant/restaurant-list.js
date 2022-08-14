1cc4afde508900388818e0dcdd84aba9030a3bdd
Jérémie Amstutz <jeremie.amstutz@gmail.com>
2022-08-14 16:37:51 +0200

    remove horizontal scroll

--- components/restaurant/restaurant-list.js
+++ components/restaurant/restaurant-list.js
@@ -1,38 +1,11 @@
 import RestaurantCard from 'components/restaurant/restaurant-card'
-import useScrollRestoration from 'hooks/useScrollRestauration'
-import { useRouter } from 'next/router'
-import { forwardRef, useImperativeHandle, useRef } from 'react'
-import slugify from 'slugify'
 
 import classes from './restaurant-list.module.css'
 
-export default forwardRef(function RestaurantList({ list, restaurants }, ref) {
-	const router = useRouter()
-
-	const listRef = useRef()
-	const resetScroll = useScrollRestoration(
-		listRef,
-		`${router.asPath === '/' ? '/home' : router.asPath}/${slugify(
-			list.name,
-			{ lower: true, remove: /[*+~.()'"!:@]/g },
-		)}`,
-	)
-
-	useImperativeHandle(ref, () => ({
-		resetScroll() {
-			resetScroll()
-		},
-	}))
-
+export default function RestaurantList({ restaurants }) {
 	return (
 		<section className={classes.container}>
-			<div className={classes.header}>
-				<h2 className={classes.name}>{list.name}</h2>
-				{list.description && (
-					<p className={classes.description}>{list.description}</p>
-				)}
-			</div>
-			<div className={classes.list} ref={listRef}>
+			<div className={classes.list}>
 				{restaurants.length < 1 ? (
 					<p style={{ margin: 0 }}>Aucun restaurant</p>
 				) : (
@@ -47,4 +20,4 @@ export default forwardRef(function RestaurantList({ list, restaurants }, ref) {
 			</div>
 		</section>
 	)
-})
+}
