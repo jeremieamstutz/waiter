46b97a6e37fd8d9783bf1db37563a4a723e4db99
Jérémie Amstutz <jeremie.amstutz@gmail.com>
2022-08-15 00:07:32 +0200

    cleaning

--- components/restaurant/restaurant-list.js
+++ components/restaurant/restaurant-list.js
@@ -4,20 +4,18 @@ import classes from './restaurant-list.module.css'
 
 export default function RestaurantList({ restaurants }) {
 	return (
-		<section className={classes.container}>
-			<div className={classes.list}>
-				{restaurants.length < 1 ? (
-					<p style={{ margin: 0 }}>Aucun restaurant</p>
-				) : (
-					restaurants.map((restaurant, index) => (
-						<RestaurantCard
-							restaurant={restaurant}
-							key={index}
-							index={index}
-						/>
-					))
-				)}
-			</div>
-		</section>
+		<div className={classes.list}>
+			{restaurants.length < 1 ? (
+				<p style={{ margin: 0 }}>Aucun restaurant</p>
+			) : (
+				restaurants.map((restaurant, index) => (
+					<RestaurantCard
+						restaurant={restaurant}
+						key={index}
+						index={index}
+					/>
+				))
+			)}
+		</div>
 	)
 }
