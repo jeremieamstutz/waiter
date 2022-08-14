e776df92e4e27f1ecaac9327b1f87196ade4c453
Jérémie Amstutz <jeremie.amstutz@gmail.com>
2022-08-14 16:19:52 +0200

    remove animation

--- components/layout/container.js
+++ components/layout/container.js
@@ -1,19 +1,9 @@
-import { motion } from 'framer-motion'
-import { fadeIn } from 'animations'
-
 import classes from './container.module.css'
 
 export default function Container({ children, ...props }) {
 	return (
-		<motion.div
-			className={classes.container}
-			initial="initial"
-			animate="animate"
-			exit="initial"
-			variants={fadeIn}
-			{...props}
-		>
+		<div className={classes.container} {...props}>
 			{children}
-		</motion.div>
+		</div>
 	)
 }
