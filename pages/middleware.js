4257149c4224cfd5d986ef0a255f098b839a6197
Jérémie Amstutz <jeremie.amstutz@gmail.com>
2022-08-14 19:35:18 +0200

    enable middleware

---
+++ pages/middleware.js
@@ -0,0 +1,23 @@
+import { NextResponse } from 'next/server'
+import acceptLanguage from 'accept-language'
+
+import { i18n } from '../next-i18next.config.js'
+
+acceptLanguage.languages(i18n.locales)
+
+const PUBLIC_FILE = /\.(.*)$/
+
+export function middleware(req) {
+	if (
+		req.nextUrl.pathname.startsWith('/_next') ||
+		req.nextUrl.pathname.includes('/api/') ||
+		PUBLIC_FILE.test(req.nextUrl.pathname)
+	) {
+		return
+	}
+	if (req.nextUrl.locale === 'default') {
+		const newUrl = req.nextUrl.clone()
+		newUrl.locale = acceptLanguage.get(req.headers.get('accept-language'))
+		return NextResponse.redirect(newUrl)
+	}
+}
