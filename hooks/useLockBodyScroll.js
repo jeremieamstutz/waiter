952b217cdffef06f488efad2ac54ac8155acae79
Jérémie Amstutz <jeremie.amstutz@gmail.com>
2022-08-16 17:01:28 +0200

    fix lock body scroll on iOS

--- hooks/useLockBodyScroll.js
+++ hooks/useLockBodyScroll.js
@@ -2,16 +2,18 @@ import { useEffect } from 'react'
 
 export default function useLockBodyScroll() {
 	useEffect(() => {
-		const { overflow, paddingRight } = window.getComputedStyle(
-			document.body,
-		)
-
+		const scrollPosition = window.pageYOffset
 		document.body.style.overflow = 'hidden'
-		// document.body.style.paddingRight = '15px'
+		document.body.style.position = 'fixed'
+		document.body.style.top = `-${scrollPosition}px`
+		document.body.style.width = '100%'
 
 		return () => {
-			document.body.style.overflow = overflow
-			// document.body.style.paddingRight = paddingRight
+			document.body.style.removeProperty('overflow')
+			document.body.style.removeProperty('position')
+			document.body.style.removeProperty('top')
+			document.body.style.removeProperty('width')
+			window.scrollTo(0, scrollPosition)
 		}
 	}, [])
 }
