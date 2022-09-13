10efd7d2e680855e5ddbbbcadb0c5ad0b26bd1f4
Jérémie Amstutz <jeremie.amstutz@gmail.com>
2022-09-13 22:40:04 +0200

    fix db

--- utils/db.js
+++ utils/db.js
@@ -3,17 +3,12 @@ import { Client, Pool, types } from 'pg'
 // Return date as String
 types.setTypeParser(1082, (value) => value)
 
-// const pool = new Pool({
-// 	connectionString: process.env.DATABASE_URL,
-// 	max: 1,
-// 	ssl: {
-// 		rejectUnauthorized: false,
-// 	},
-// })
+let pool
 
-export async function query(text, values) {
-	const client = new Client({
+if (!pool) {
+	pool = new Pool({
 		connectionString: process.env.DATABASE_URL,
+		max: 5,
 		ssl:
 			process.env.NODE_ENV === 'production'
 				? {
@@ -21,47 +16,27 @@ export async function query(text, values) {
 				  }
 				: undefined,
 	})
+}
 
+export async function query(text, values) {
 	try {
-		await client.connect()
+		// const start = Date.now()
 
-		const start = Date.now()
-		// const result = await pool.query(text, values)
+		const client = await pool.connect()
 		const result = await client.query(text, values)
-		const duration = Date.now() - start
+		await client.release()
 
-		await client.end()
+		// const duration = Date.now() - start
+
 		// console.log('Query: ', {
 		// 	text,
 		// 	values,
 		// 	duration,
 		// 	rows: result.rowCount,
 		// })
+		
 		return result
 	} catch (error) {
 		console.log(error)
 	}
-}
-
-export async function getClient() {
-	const client = await pool.connect()
-	const query = client.query
-	const release = client.release
-	const timeout = setTimeout(() => {
-		console.error('A client has been checked out for more than 5 seconds!')
-		console.error(
-			`The last executed query on this client was: ${client.lastQuery}`,
-		)
-	}, 5000)
-	client.query = (...args) => {
-		client.lastQuery = args
-		return query.apply(client, args)
-	}
-	client.release = () => {
-		clearTimeout(timeout)
-		client.query = query
-		client.release = release
-		return release.apply(client)
-	}
-	return client
 }
