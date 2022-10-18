faccc043a2edd53edae3e8c7b28e9530a7756ca9
Jérémie Amstutz <jeremie.amstutz@gmail.com>
2022-10-18 15:33:43 +0200

    add beta superscript

--- components/icons/logo.js
+++ components/icons/logo.js
@@ -2,6 +2,7 @@ export default function Logo() {
 	return (
 		<div
 			style={{
+				position: 'relative',
 				display: 'flex',
 				alignItems: 'center',
 				gap: '0.5rem',
@@ -16,6 +17,18 @@ export default function Logo() {
 				}}
 			/>
 			<div style={{ fontSize: '1.5rem' }}>waiter</div>
+			<span
+				style={{
+					position: 'absolute',
+					top: 0,
+					right: -4,
+					translate: '100% 0',
+					fontSize: '0.8rem',
+					color: '#666',
+				}}
+			>
+				beta
+			</span>
 		</div>
 	)
 }
