792ce56a29b1d58ffcb931edd6c3b0e861de02ee
Jérémie Amstutz <jeremie.amstutz@gmail.com>
2022-10-18 15:34:02 +0200

    add support for both types

--- components/icons/plus.js
+++ components/icons/plus.js
@@ -1,18 +1,40 @@
-export default function Plus(props) {
-	return (
-		<svg
-			xmlns="http://www.w3.org/2000/svg"
-			width={20}
-			height={20}
-			viewBox="0 0 20 20"
-			fill="currentColor"
-			{...props}
-		>
-			<path
-				fillRule="evenodd"
-				d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
-				clipRule="evenodd"
-			/>
-		</svg>
-	)
+export default function PlusIcon(props) {
+	switch (props.type || 'solid') {
+		case 'solid':
+			return (
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width={20}
+					height={20}
+					viewBox="0 0 20 20"
+					fill="currentColor"
+					{...props}
+				>
+					<path
+						fillRule="evenodd"
+						d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z"
+						clipRule="evenodd"
+					/>
+				</svg>
+			)
+		case 'outline':
+			return (
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width={24}
+					height={24}
+					viewBox="0 0 24 24"
+					stroke="currentColor"
+					strokeWidth={2}
+					fill="none"
+					{...props}
+				>
+					<path
+						strokeLinecap="round"
+						strokeLinejoin="round"
+						d="M12 4v16m8-8H4"
+					/>
+				</svg>
+			)
+	}
 }
