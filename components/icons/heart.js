a9f1239bac97a28ba9a245c55250401816c5c481
Jérémie Amstutz <jeremie.amstutz@gmail.com>
2022-10-18 15:32:40 +0200

    add heart icon

---
+++ components/icons/heart.js
@@ -0,0 +1,40 @@
+export default function HeartIcon(props) {
+	switch (props.type || 'solid') {
+		case 'solid':
+			return (
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width={20}
+					height={20}
+					viewBox="0 0 20 20"
+					fill="currentColor"
+					{...props}
+				>
+					<path
+						fillRule="evenodd"
+						d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z"
+						clipRule="evenodd"
+					/>
+				</svg>
+			)
+		case 'outline':
+			return (
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width={24}
+					height={24}
+					viewBox="0 0 24 24"
+					stroke="currentColor"
+					strokeWidth={2}
+					fill="none"
+					{...props}
+				>
+					<path
+						strokeLinecap="round"
+						strokeLinejoin="round"
+						d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+					/>
+				</svg>
+			)
+	}
+}
