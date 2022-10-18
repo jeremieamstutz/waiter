289c89e372e11eb374d6fb171619498073871eac
Jérémie Amstutz <jeremie.amstutz@gmail.com>
2022-10-18 16:18:08 +0200

    add general layout

---
+++ components/layout/layout.js
@@ -0,0 +1,49 @@
+import Head from 'next/head'
+import Container from './container'
+import Footer from './footer'
+import Header from './header'
+import Main from './main'
+
+export default function Layout({ head, children }) {
+	return (
+		<>
+			<Head>
+				<title>
+					{head?.title ? `${head.title} • Waiter` : 'Waiter'}
+				</title>
+				{head?.meta?.description && (
+					<meta name="description" content={head.meta.description} />
+				)}
+				{head?.title && (
+					<meta
+						property="og:title"
+						content={`${head.title} • Waiter`}
+					/>
+				)}
+				{head?.meta?.description && (
+					<meta
+						property="og:description"
+						content={head.meta.description}
+					/>
+				)}
+				{head?.meta?.image && (
+					<meta property="og:image" content={head.meta.image} />
+				)}
+				{head?.meta?.url && (
+					<meta property="og:url" content={head.meta.url} />
+				)}
+				{head?.meta?.type && (
+					<meta property="og:type" content={head.meta.type} />
+				)}
+				{head?.meta?.data && (
+					<script type="application/ld+json">{head.meta.data}</script>
+				)}
+			</Head>
+			<Container>
+				<Header />
+				<Main>{children}</Main>
+				<Footer />
+			</Container>
+		</>
+	)
+}
--- components/layout/main.js
+++ components/layout/main.js
@@ -1,9 +1,8 @@
+import classes from './main.module.css'
+
 export default function Main({ children, ...props }) {
 	return (
-		<main
-			style={{ flex: 1, display: 'flex', flexDirection: 'column' }}
-			{...props}
-		>
+		<main className={classes.main} {...props}>
 			{children}
 		</main>
 	)
