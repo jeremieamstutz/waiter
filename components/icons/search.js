7431b5a2e632854e8e8bcf3d19c4ee0d5c4e4ba0
Jérémie Amstutz <jeremie.amstutz@gmail.com>
2022-10-18 15:34:21 +0200

    add support for both types

--- components/icons/search.js
+++ components/icons/search.js
@@ -1,18 +1,40 @@
-export default function Search(props) {
-	return (
-		<svg
-			xmlns="http://www.w3.org/2000/svg"
-			width={20}
-			height={20}
-			viewBox="0 0 20 20"
-			fill="currentColor"
-			{...props}
-		>
-			<path
-				fillRule="evenodd"
-				d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z"
-				clipRule="evenodd"
-			/>
-		</svg>
-	)
+export default function SearchIcon(props) {
+	switch (props.type || 'solid') {
+		case 'solid':
+			return (
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width={20}
+					height={20}
+					viewBox="0 0 20 20"
+					fill="currentColor"
+					{...props}
+				>
+					<path
+						fillRule="evenodd"
+						d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+						clipRule="evenodd"
+					/>
+				</svg>
+			)
+		case 'outline':
+			return (
+				<svg
+					xmlns="http://www.w3.org/2000/svg"
+					width={24}
+					height={24}
+					viewBox="0 0 24 24"
+					stroke="currentColor"
+					strokeWidth={2}
+					fill="none"
+					{...props}
+				>
+					<path
+						strokeLinecap="round"
+						strokeLinejoin="round"
+						d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+					/>
+				</svg>
+			)
+	}
 }
